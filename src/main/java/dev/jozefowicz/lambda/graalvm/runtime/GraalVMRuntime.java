38fc3dbc2f97f1268045b0a27b6a191a50ff2ea6
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-03-05 22:02:57 +0100

    fixing reflection config

--- src/main/java/dev/jozefowicz/lambda/graalvm/runtime/GraalVMRuntime.java
+++ src/main/java/dev/jozefowicz/lambda/graalvm/runtime/GraalVMRuntime.java
@@ -1,7 +1,9 @@
 package dev.jozefowicz.lambda.graalvm.runtime;
 
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import java.lang.management.ManagementFactory;
 import java.net.URI;
@@ -16,8 +18,8 @@ public class GraalVMRuntime {
     private static final String AWS_LAMBDA_RUNTIME_API = System.getenv("AWS_LAMBDA_RUNTIME_API");
     private static final String LAMBDA_NEXT_INVOCATION_ENDPOINT = "http://" + AWS_LAMBDA_RUNTIME_API + "/2018-06-01/runtime/invocation/next";
 
-    private final HttpClient httpClient = HttpClient.newHttpClient();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
 
     private final RequestHandler requestHandler;
     private final Class<?> eventClass;
@@ -25,15 +27,18 @@ public class GraalVMRuntime {
     public GraalVMRuntime(RequestHandler requestHandler, Class<?> eventClass) {
         this.requestHandler = requestHandler;
         this.eventClass = eventClass;
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
+        this.httpClient = HttpClient.newHttpClient();
     }
 
     public void execute() {
         System.out.println("Bootstrap time: " + ManagementFactory.getRuntimeMXBean().getUptime());
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
         while (true) {
             try {
                 final var startTimestamp = System.currentTimeMillis();
-                System.out.println(LAMBDA_NEXT_INVOCATION_ENDPOINT);
                 final HttpRequest newInvocationRequest = HttpRequest.newBuilder().uri(new URI(LAMBDA_NEXT_INVOCATION_ENDPOINT)).timeout(Duration.ofSeconds(2)).build();
                 final HttpResponse<String> invocationRequest = httpClient.send(newInvocationRequest, HttpResponse.BodyHandlers.ofString());
                 var invocationID = invocationRequest.headers().map().get("Lambda-Runtime-Aws-Request-Id").get(0);
@@ -52,7 +57,8 @@ public class GraalVMRuntime {
                 System.out.println("Lambda result endpoint response: " + invocationResultResponse.body());
                 System.out.println("Execution time: " + (System.currentTimeMillis() - startTimestamp));
             } catch (Exception e) {
-                // who cares
+                System.out.println(String.format("Exception while executing handler logic: %s", e.getLocalizedMessage()));
+                e.printStackTrace();
             }
         }
     }
